f818268224121f7e11e0e8313b39bb08a13ea6b2
Евгений <137326840+Belkub@users.noreply.github.com>
2026-01-06 16:51:15 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+
+
+import pickle
+import streamlit as st
+import pandas as pd
+import speech_recognition as sr
+
+
+st.title('Bentonite Co')
+
+
+st.subheader("Сервис предназначен для комплексного определения качества бентонита с точки зрения эффективности модификации катионными ПАВ на основе сочетания API-теста бентонитовой суспензии и физико-химических свойств глинистого минерала")
+
+
+
+
+    
+
+
+
