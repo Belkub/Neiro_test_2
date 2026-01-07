9110597a11ece45caa87827626a0922f1ecf25a3
Евгений <137326840+Belkub@users.noreply.github.com>
2026-01-07 16:27:32 +0300

    Update start.py

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ img = Image.open("bent.png")
 st.image(img, width=200)
 #st.image(img, use_column_width=True)
 #st.title('Bentonite Co')
-st.title('AI bentonite quality calc')
+st.title('Bentonite quality calc')
 
 st.subheader("Сервис предназначен для комплексного определения качества бентонита с точки зрения эффективности модификации катионными ПАВ на основе сочетания API-теста бентонитовой суспензии и физико-химических свойств глинистого минерала")
 
